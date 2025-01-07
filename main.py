330a7b97609f06a6ac9a22a4de3e11b1bd45bd8d
Cem Levent Avcı <cemlevent54@gmail.com>
2025-01-07 19:16:38 +0300

    UI forms added.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import sys
 from PyQt5 import QtWidgets
+
 from mainFiles.mainForm import Ui_DownloadMate
 from youtubeFiles.youtubeDownloadForm import Ui_MainWindow
+from twitterFiles.twitterDownloadForm import Ui_TwitterDownloader
+from instagramFiles.instagramDownloadForm import Ui_InstagramLoader
 
 
 class MainForm(QtWidgets.QMainWindow):
@@ -12,12 +15,27 @@ class MainForm(QtWidgets.QMainWindow):
 
         # YouTube Downloader butonuna tıklama olayını tanımlıyoruz.
         self.ui.youtubeDownloader.clicked.connect(self.openYoutubeDownloadForm)
-
+        self.ui.twitterDownloader.clicked.connect(self.openTwitterDownloadForm)
+        self.ui.instagramDownloader.clicked.connect(self.openInstagramDownloadForm)
+        
+        
     def openYoutubeDownloadForm(self):
         # YouTube Download Form'u aç ve MainForm'u kapat
         self.youtubeDownloadForm = YoutubeDownloadForm(self)
         self.youtubeDownloadForm.show()
         self.hide()  # MainForm'u kapatmak yerine gizliyoruz
+    
+    def openInstagramDownloadForm(self):
+        # Instagram Download Form'u aç ve MainForm'u kapat
+        self.instagramDownloadForm = InstagramDownloadForm(self)
+        self.instagramDownloadForm.show()
+        self.hide()
+    
+    def openTwitterDownloadForm(self):
+        # Twitter Download Form'u aç ve MainForm'u kapat
+        self.twitterDownloadForm = TwitterDownloadForm(self)
+        self.twitterDownloadForm.show()
+        self.hide()
 
 
 class YoutubeDownloadForm(QtWidgets.QMainWindow):
@@ -32,6 +50,35 @@ class YoutubeDownloadForm(QtWidgets.QMainWindow):
         if self.parent:
             self.parent.show()
         event.accept()  # Kapatma olayını kabul ediyoruz
+
+class InstagramDownloadForm(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__()
+        self.ui = Ui_InstagramLoader()
+        self.ui.setupUi(self)
+        self.parent = parent  # MainForm'u referans olarak alıyoruz
+
+    def closeEvent(self, event):
+        # InstagramDownloadForm kapanırken MainForm'u tekrar gösteriyoruz
+        if self.parent:
+            self.parent.show()
+        event.accept()  # Kapatma olayını kabul ediyoruz
+
+class TwitterDownloadForm(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__()
+        self.ui = Ui_TwitterDownloader()
+        self.ui.setupUi(self)
+        self.parent = parent  # MainForm'u referans olarak alıyoruz
+
+    def closeEvent(self, event):
+        # TwitterDownloadForm kapanırken MainForm'u tekrar gösteriyoruz
+        if self.parent:
+            self.parent.show()
+        event.accept()  # Kapatma olayını kabul ediyoruz
+        
+
+
 
 
 if __name__ == "__main__":
--- youtubeFiles/youtubeDownloadForm.py
+++ youtubeFiles/youtubeDownloadForm.py
@@ -18,14 +18,11 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("Youtube Downloader")
         MainWindow.resize(800, 600)
-        MainWindow.setMinimumSize(QtCore.QSize(800, 600))
         MainWindow.setMaximumSize(QtCore.QSize(800, 600))
+        MainWindow.setStyleSheet("QMainWindow {background-color: #CD201F;}")
         font = QtGui.QFont()
-        font.setFamily("Segoe Script")
+        font.setFamily("Rockwell Extra Bold")
         MainWindow.setFont(font)
-        MainWindow.setStyleSheet("QMainWindow {\n"
-"                background-color: #FFCCCC; /* Hafif kırmızı arka plan */\n"
-"            }")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         
@@ -33,9 +30,11 @@ class Ui_MainWindow(object):
         self.headerLabel = QtWidgets.QLabel(self.centralwidget)
         self.headerLabel.setGeometry(QtCore.QRect(270, 70, 271, 81))
         font = QtGui.QFont()
-        font.setFamily("Segoe Script")
+        font.setFamily("Rockwell Extra Bold")
         font.setPointSize(10)
         self.headerLabel.setFont(font)
+        self.headerLabel.setStyleSheet("QLabel {color: white;font-weight: 900;\n"
+"};")
         self.headerLabel.setObjectName("headerLabel")
         
         # Link text box
@@ -43,6 +42,7 @@ class Ui_MainWindow(object):
         self.linkTxtBox.setGeometry(QtCore.QRect(50, 150, 551, 41))
         font = QtGui.QFont()
         font.setPointSize(10)
+        font.setFamily("Rockwell")
         self.linkTxtBox.setFont(font)
         self.linkTxtBox.setObjectName("linkTxtBox")
         
@@ -50,53 +50,57 @@ class Ui_MainWindow(object):
         self.formatGroupBox = QtWidgets.QGroupBox(self.centralwidget)
         self.formatGroupBox.setGeometry(QtCore.QRect(40, 230, 171, 161))
         font = QtGui.QFont()
-        font.setFamily("Segoe Script")
+        font.setFamily("Rockwell Extra Bold")
         self.formatGroupBox.setFont(font)
+        self.formatGroupBox.setStyleSheet("color: white;font-weight: 900;\n"
+"")
         self.formatGroupBox.setObjectName("formatGroupBox")
         
         # Format group box - mp4 radio button
         self.mp4Radio = QtWidgets.QRadioButton(self.formatGroupBox)
         self.mp4Radio.setGeometry(QtCore.QRect(20, 80, 141, 20))
         font = QtGui.QFont()
-        font.setFamily("Segoe Script")
+        font.setFamily("Rockwell Extra Bold")
         self.mp4Radio.setFont(font)
         self.mp4Radio.setObjectName("mp4Radio")
+        self.mp4Radio.setStyleSheet("color: white;font-weight: 900;")
         self.mp4Radio.setChecked(False)
         
         # Format group box - mp3 radio button
         self.mp3Radio = QtWidgets.QRadioButton(self.formatGroupBox)
         self.mp3Radio.setGeometry(QtCore.QRect(20, 40, 141, 20))
         font = QtGui.QFont()
-        font.setFamily("Segoe Script")
+        font.setFamily("Rockwell Extra Bold")
         self.mp3Radio.setFont(font)
         self.mp3Radio.setObjectName("mp3Radio")
+        self.mp3Radio.setStyleSheet("color: white;font-weight: 900;")
         self.mp3Radio.setChecked(False)
         
         # Search button
         self.searchButton = QtWidgets.QPushButton(self.centralwidget)
         self.searchButton.setGeometry(QtCore.QRect(630, 150, 121, 41))
         font = QtGui.QFont()
-        font.setFamily("Segoe Script")
+        font.setFamily("Rockwell Extra Bold")
         font.setPointSize(14)
         font.setBold(True)
         font.setWeight(75)
         self.searchButton.setFont(font)
         self.searchButton.setStyleSheet("QPushButton {\n"
 "                background-color: #FFFFFF; /* Beyaz arka plan */\n"
-"                color: #FF0000; /* Kırmızı yazı */\n"
+"                color: #CD201F; /* Kırmızı yazı */\n"
 "                font-size: 14px;\n"
 "                font-weight: bold;\n"
-"                border: 2px solid #FF0000; /* Kırmızı kenar */\n"
+"                border: 2px solid #CD201F; /* Kırmızı kenar */\n"
 "                border-radius: 8px; /* Hafif yuvarlak kenarlar */\n"
 "                padding: 6px;\n"
 "            }\n"
 "            QPushButton:hover {\n"
-"                background-color: #FF4C4C; /* Hover sırasında açık kırmızı */\n"
+"                background-color: #657786; /* Hover sırasında açık kırmızı */\n"
 "                color: #FFFFFF; /* Yazı beyaz */\n"
-"                border: 2px solid #FF0000;\n"
+"                border: 2px solid #383838;\n"
 "            }\n"
 "            QPushButton:pressed {\n"
-"                background-color: #CC0000; /* Basılınca koyu kırmızı */\n"
+"                background-color: #657786; /* Basılınca koyu kırmızı */\n"
 "                color: #FFFFFF;\n"
 "            }")
         self.searchButton.setObjectName("searchButton")
@@ -105,71 +109,76 @@ class Ui_MainWindow(object):
         self.qualityGroupbox = QtWidgets.QGroupBox(self.centralwidget)
         self.qualityGroupbox.setGeometry(QtCore.QRect(250, 230, 201, 221))
         font = QtGui.QFont()
-        font.setFamily("Segoe Script")
+        font.setFamily("Rockwell Extra Bold")
         self.qualityGroupbox.setFont(font)
+        self.qualityGroupbox.setStyleSheet("color: white;font-weight: 900;\n")
         self.qualityGroupbox.setObjectName("qualityGroupbox")
         
         # Quality Radio Buttons (360p)
         self.radio360p = QtWidgets.QRadioButton(self.qualityGroupbox)
         self.radio360p.setGeometry(QtCore.QRect(40, 80, 141, 20))
         font = QtGui.QFont()
-        font.setFamily("Segoe Script")
+        font.setFamily("Rockwell Extra Bold")
         self.radio360p.setFont(font)
         self.radio360p.setObjectName("radio360p")
+        self.radio360p.setStyleSheet("color: white;font-weight: 900;")
         self.radio360p.setChecked(False)
         
         # Quality Radio Buttons (480p)
         self.radio480p = QtWidgets.QRadioButton(self.qualityGroupbox)
         self.radio480p.setGeometry(QtCore.QRect(40, 40, 141, 20))
         font = QtGui.QFont()
-        font.setFamily("Segoe Script")
+        font.setFamily("Rockwell Extra Bold")
         self.radio480p.setFont(font)
         self.radio480p.setObjectName("radio480p")
+        self.radio480p.setStyleSheet("color: white;font-weight: 900;")
         self.radio480p.setChecked(False)        
         
         # Quality Radio Buttons (720p)
         self.radio720p = QtWidgets.QRadioButton(self.qualityGroupbox)
         self.radio720p.setGeometry(QtCore.QRect(40, 120, 141, 20))
         font = QtGui.QFont()
-        font.setFamily("Segoe Script")
+        font.setFamily("Rockwell Extra Bold")
         self.radio720p.setFont(font)
         self.radio720p.setObjectName("radio720p")
+        self.radio720p.setStyleSheet("color: white;font-weight: 900;")
         self.radio720p.setChecked(False)
         
         # Quality Radio Buttons (1080p)
         self.radio1080p = QtWidgets.QRadioButton(self.qualityGroupbox)
         self.radio1080p.setGeometry(QtCore.QRect(40, 160, 141, 20))
         font = QtGui.QFont()
-        font.setFamily("Segoe Script")
+        font.setFamily("Rockwell Extra Bold")
         self.radio1080p.setFont(font)
         self.radio1080p.setObjectName("radio1080p")
+        self.radio1080p.setStyleSheet("color: white;font-weight: 900;")
         self.radio1080p.setChecked(False)
         
         # Downloads label
         self.downloadsFolderButton = QtWidgets.QPushButton(self.centralwidget)
         self.downloadsFolderButton.setGeometry(QtCore.QRect(630, 400, 121, 41))
         font = QtGui.QFont()
-        font.setFamily("Segoe Script")
+        font.setFamily("Rockwell Extra Bold")
         font.setPointSize(14)
         font.setBold(True)
         font.setWeight(75)
         self.downloadsFolderButton.setFont(font)
         self.downloadsFolderButton.setStyleSheet("QPushButton {\n"
 "                background-color: #FFFFFF; /* Beyaz arka plan */\n"
-"                color: #FF0000; /* Kırmızı yazı */\n"
+"                color: #CD201F; /* Kırmızı yazı */\n"
 "                font-size: 14px;\n"
 "                font-weight: bold;\n"
-"                border: 2px solid #FF0000; /* Kırmızı kenar */\n"
+"                border: 2px solid #CD201F; /* Kırmızı kenar */\n"
 "                border-radius: 8px; /* Hafif yuvarlak kenarlar */\n"
 "                padding: 6px;\n"
 "            }\n"
 "            QPushButton:hover {\n"
-"                background-color: #FF4C4C; /* Hover sırasında açık kırmızı */\n"
+"                background-color: #657786; /* Hover sırasında açık kırmızı */\n"
 "                color: #FFFFFF; /* Yazı beyaz */\n"
-"                border: 2px solid #FF0000;\n"
+"                border: 2px solid #383838;\n"
 "            }\n"
 "            QPushButton:pressed {\n"
-"                background-color: #CC0000; /* Basılınca koyu kırmızı */\n"
+"                background-color: #657786; /* Basılınca koyu kırmızı */\n"
 "                color: #FFFFFF;\n"
 "            }")
         self.downloadsFolderButton.setObjectName("downloadsFolderButton")
@@ -179,7 +188,7 @@ class Ui_MainWindow(object):
         self.resultLabel = QtWidgets.QLabel(self.centralwidget)
         self.resultLabel.setGeometry(QtCore.QRect(580, 240, 151, 81))
         font = QtGui.QFont()
-        font.setFamily("Segoe Script")
+        font.setFamily("Rockwell Extra Bold")
         self.resultLabel.setFont(font)
         self.resultLabel.setText("")
         self.resultLabel.setObjectName("resultLabel")
@@ -205,7 +214,7 @@ class Ui_MainWindow(object):
         self.formatGroupBox.setTitle(_translate("MainWindow", "Format"))
         self.mp4Radio.setText(_translate("MainWindow", "mp4"))
         self.mp3Radio.setText(_translate("MainWindow", "mp3"))
-        self.searchButton.setText(_translate("MainWindow", "Search Link"))
+        self.searchButton.setText(_translate("MainWindow", "Download"))
         self.qualityGroupbox.setTitle(_translate("MainWindow", "Quality"))
         self.radio360p.setText(_translate("MainWindow", "480p"))
         self.radio480p.setText(_translate("MainWindow", "360p"))
@@ -222,13 +231,13 @@ class Ui_MainWindow(object):
     def get_selected_quality(self):
         """Seçilen kaliteyi döndürür."""
         if self.radio360p.isChecked():
-            return "134"  # 360p kalite kodu
+            return "134"  
         elif self.radio480p.isChecked():
-            return "135"  # 480p kalite kodu
+            return "135"  
         elif self.radio720p.isChecked():
-            return "136"  # 720p kalite kodu
+            return "136"  
         elif self.radio1080p.isChecked():
-            return "137"  # 1080p kalite kodu
+            return "137"  
         return None   
     
     def download_video(self):
@@ -301,11 +310,4 @@ class Ui_MainWindow(object):
         
         os.startfile(downloads_folder)
 
-# if __name__ == "__main__":
-#     import sys
-#     app = QtWidgets.QApplication(sys.argv)
-#     MainWindow = QtWidgets.QMainWindow()
-#     ui = Ui_MainWindow()
-#     ui.setupUi(MainWindow)
-#     MainWindow.show()
-#     sys.exit(app.exec_())
+
