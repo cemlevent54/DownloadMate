451cd306818ec35f2684c62ad664b9b29dcb7142
cemlevent54 <cemlevent54@gmail.com>
2025-03-23 01:04:18 +0300

    ffmpeg

--- youtubeFiles/youtubeDownloader.py
+++ youtubeFiles/youtubeDownloader.py
@@ -3,24 +3,41 @@ import subprocess
 import os
 import shutil
 import re  # Karakter temizleme için
+import FFmpegHelper.FFmpegHelper as FFmpegHelper
 
 class YouTubeDownloader:
     def __init__(self):
         self.downloads_folder = self.create_downloads_folder()
         self.ffmpeg_path = self.check_ffmpeg_path()
+        self.ffmpeg_helper = FFmpegHelper.FFmpegHelper()
+        print("FFmpegHelper: ", self.ffmpeg_helper)
 
     def check_ffmpeg_path(self):
         """
         FFmpeg'in mevcut olup olmadığını kontrol eder ve yolunu döner.
         """
-        ffmpeg_path = shutil.which("ffmpeg")
+        # ffmpeg_path = shutil.which("ffmpeg")
+        ffmpeg_path = os.path.join(os.getcwd(), "setup", "ffmpeg", "bin", "ffmpeg.exe") 
         if ffmpeg_path:
             print(f"FFmpeg bulundu: {ffmpeg_path}")
-            return ffmpeg_path
         else:
-            ffmpeg_path = "C:/ffmpeg/bin/ffmpeg.exe"
+            # FFmpeg, sistem PATH'inde değilse, manuel olarak yolu belirliyoruz
+            ffmpeg_path = os.path.join(os.getcwd(), "setup", "ffmpeg", "bin", "ffmpeg.exe")
             print(f"FFmpeg manuel olarak ayarlandı: {ffmpeg_path}")
-            return ffmpeg_path
+
+            # FFmpeg'in yüklü olmadığını tespit ettiğimizde PATH'e ekliyoruz
+            if not self.ffmpeg_helper.is_user_admin():
+                print("Yönetici izni gerekiyor. Yönetici olarak çalıştırmak ister misiniz?")
+                if self.ffmpeg_helper.ask_for_admin_permission():
+                    self.ffmpeg_helper.add_ffmpeg_to_path()  # FFmpeg yolunu PATH'e ekle
+                    print("FFmpeg PATH'e eklendi.")
+                else:
+                    print("Yönetici izni verilmedi. FFmpeg PATH'e eklenemedi.")
+            else:
+                # Eğer yönetici izni varsa, manuel olarak PATH'e ekleyelim
+                self.add_ffmpeg_to_system_path(ffmpeg_path)
+                    
+        return ffmpeg_path
 
     def create_downloads_folder(self):
         """
@@ -109,6 +126,10 @@ class YouTubeDownloader:
         """
         Video ve ses dosyalarını birleştirir ve geçici dosyaları temizler.
         """
+        print("FFmpeg: ", self.ffmpeg_path)
+        print(f"Video dosyası tam yolu: {os.path.abspath(video_file)}")
+        print(f"Ses dosyası tam yolu: {os.path.abspath(audio_file)}")
+        print(f"Çıktı dosyası tam yolu: {os.path.abspath(output_file)}")
         if not self.ffmpeg_path:
             print("Hata: FFmpeg yüklü değil veya bulunamadı.")
             return
@@ -116,8 +137,19 @@ class YouTubeDownloader:
         print("Video ve ses birleştiriliyor...")
         try:
             subprocess.run([
-                self.ffmpeg_path, '-i', video_file, '-i', audio_file, '-c:v', 'copy', '-c:a', 'aac', output_file
-            ], check=True)
+                self.ffmpeg_path,   # FFmpeg'in yolda bulunduğu dosya
+                '-i', video_file,   # Video dosyasının yolu
+                '-i', audio_file,   # Ses dosyasının yolu
+                '-c:v', 'copy',     # Video codec'ini kopyala
+                '-c:a', 'aac',      # Ses codec'ini AAC olarak ayarla
+                '-b:a', '192k',     # Sesin bit hızını 192kbit/s olarak ayarla
+                '-shortest',        # En kısa dosyaya göre kes
+                '-y',               # Var olan dosyayı zorla üzerine yaz
+                '-loglevel', 'error', # Hata mesajlarını göster
+                output_file         # Çıktı dosyasının adı
+            ], check=True, capture_output=True, text=True)
+
+
             print(f"Birleştirme tamamlandı! Çıktı dosyası: {output_file}")
 
             # Geçici dosyaları sil
